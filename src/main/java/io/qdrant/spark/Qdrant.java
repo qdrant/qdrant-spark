eb95366a120a61663ada1206bdd394519c066508
Anush008 <anushshetty90@gmail.com>
2024-02-09 11:21:03 +0530

    chore: improve options handling Qdrant.java

--- src/main/java/io/qdrant/spark/Qdrant.java
+++ src/main/java/io/qdrant/spark/Qdrant.java
@@ -11,13 +11,13 @@ import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.util.CaseInsensitiveStringMap;
 
 /**
- * A class that implements the TableProvider and DataSourceRegister interfaces. Provides methods to
+ * A class that implements the TableProvider and DataSourceRegister interfaces.
+ * Provides methods to
  * infer schema, get table, and check required options.
  */
 public class Qdrant implements TableProvider, DataSourceRegister {
 
-  private final String[] requiredFields =
-      new String[] {"schema", "collection_name", "embedding_field", "qdrant_url"};
+  private final String[] requiredFields = new String[] { "schema", "collection_name", "embedding_field", "qdrant_url" };
 
   /**
    * Returns the short name of the data source.
@@ -37,20 +37,24 @@ public class Qdrant implements TableProvider, DataSourceRegister {
    */
   @Override
   public StructType inferSchema(CaseInsensitiveStringMap options) {
-
+    for (String fieldName : requiredFields) {
+      if (!options.containsKey(fieldName)) {
+        throw new IllegalArgumentException(fieldName.concat(" option is required"));
+      }
+    }
     StructType schema = (StructType) StructType.fromJson(options.get("schema"));
-    checkRequiredOptions(options, schema);
+    validateOptions(options, schema);
 
     return schema;
-  }
-  ;
+  };
 
   /**
-   * Returns a table for the data source based on the provided schema, partitioning, and properties.
+   * Returns a table for the data source based on the provided schema,
+   * partitioning, and properties.
    *
-   * @param schema The schema of the table.
+   * @param schema       The schema of the table.
    * @param partitioning The partitioning of the table.
-   * @param properties The properties of the table.
+   * @param properties   The properties of the table.
    * @return The table for the data source.
    */
   @Override
@@ -61,18 +65,15 @@ public class Qdrant implements TableProvider, DataSourceRegister {
   }
 
   /**
-   * Checks if the required options are present in the provided options and if the id_field and
-   * embedding_field options are present in the provided schema.
+   * Checks if the required options are present in the provided options and chekcs
+   * if the specified
+   * id_field and
+   * embedding_field are present in the provided schema.
    *
    * @param options The options to check.
-   * @param schema The schema to check.
+   * @param schema  The schema to check.
    */
-  void checkRequiredOptions(CaseInsensitiveStringMap options, StructType schema) {
-    for (String fieldName : requiredFields) {
-      if (!options.containsKey(fieldName)) {
-        throw new IllegalArgumentException(fieldName.concat(" option is required"));
-      }
-    }
+  void validateOptions(CaseInsensitiveStringMap options, StructType schema) {
 
     List<String> fieldNames = Arrays.asList(schema.fieldNames());
 
@@ -80,14 +81,14 @@ public class Qdrant implements TableProvider, DataSourceRegister {
       String idField = options.get("id_field").toString();
 
       if (!fieldNames.contains(idField)) {
-        throw new IllegalArgumentException("id_field option is not present in the schema");
+        throw new IllegalArgumentException("Specified 'id_field' is not present in the schema");
       }
     }
 
     String embeddingField = options.get("embedding_field").toString();
 
     if (!fieldNames.contains(embeddingField)) {
-      throw new IllegalArgumentException("embedding_field option is not present in the schema");
+      throw new IllegalArgumentException("Specified 'embedding_field' is not present in the schema");
     }
   }
 }
