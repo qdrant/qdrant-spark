5a2d5637ad8b9e49bdb6db65199c2b26ca5377e4
Anush008 <anushshetty90@gmail.com>
2024-03-02 16:49:36 +0530

    chore: formatting

--- src/main/java/io/qdrant/spark/QdrantPayloadHandler.java
+++ src/main/java/io/qdrant/spark/QdrantPayloadHandler.java
@@ -2,28 +2,27 @@ package io.qdrant.spark;
 
 import static io.qdrant.spark.QdrantValueFactory.value;
 
+import io.qdrant.client.grpc.JsonWithInt.Value;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 
-import io.qdrant.client.grpc.JsonWithInt.Value;
-
 public class QdrantPayloadHandler {
-    static Map<String, Value> preparePayload(InternalRow record, StructType schema, QdrantOptions options) {
+  static Map<String, Value> preparePayload(
+      InternalRow record, StructType schema, QdrantOptions options) {
 
-        Map<String, Value> payload = new HashMap<>();
-        for (StructField field : schema.fields()) {
+    Map<String, Value> payload = new HashMap<>();
+    for (StructField field : schema.fields()) {
 
-            if (options.payloadFieldsToSkip.contains(field.name())) {
-                continue;
-            }
-            int fieldIndex = schema.fieldIndex(field.name());
-            payload.put(field.name(), value(record, field, fieldIndex));
-        }
-
-        return payload;
+      if (options.payloadFieldsToSkip.contains(field.name())) {
+        continue;
+      }
+      int fieldIndex = schema.fieldIndex(field.name());
+      payload.put(field.name(), value(record, field, fieldIndex));
     }
+
+    return payload;
+  }
 }
--- src/main/java/io/qdrant/spark/QdrantVectorHandler.java
+++ src/main/java/io/qdrant/spark/QdrantVectorHandler.java
@@ -3,72 +3,73 @@ package io.qdrant.spark;
 import static io.qdrant.client.VectorFactory.vector;
 import static io.qdrant.client.VectorsFactory.namedVectors;
 
+import com.google.common.primitives.Floats;
+import com.google.common.primitives.Ints;
+import io.qdrant.client.grpc.Points.Vector;
+import io.qdrant.client.grpc.Points.Vectors;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.types.StructType;
 
-import com.google.common.primitives.Floats;
-import com.google.common.primitives.Ints;
-
-import io.qdrant.client.grpc.Points.Vector;
-import io.qdrant.client.grpc.Points.Vectors;
-
 public class QdrantVectorHandler {
-    static Vectors prepareVectors(InternalRow record, StructType schema, QdrantOptions options) {
+  static Vectors prepareVectors(InternalRow record, StructType schema, QdrantOptions options) {
 
-        Vectors.Builder vectorsBuilder = Vectors.newBuilder();
-        Vectors sparseVectors = prepareSparseVectors(record, schema, options);
-        Vectors denseVectors = prepareDenseVectors(record, schema, options);
+    Vectors.Builder vectorsBuilder = Vectors.newBuilder();
+    Vectors sparseVectors = prepareSparseVectors(record, schema, options);
+    Vectors denseVectors = prepareDenseVectors(record, schema, options);
 
-        vectorsBuilder.mergeFrom(sparseVectors).mergeFrom(denseVectors);
+    vectorsBuilder.mergeFrom(sparseVectors).mergeFrom(denseVectors);
 
-        if (options.embeddingField.isEmpty()) {
-            return vectorsBuilder.build();
-        }
-
-        int vectorFieldIndex = schema.fieldIndex(options.embeddingField.trim());
-        float[] embeddings = record.getArray(vectorFieldIndex).toFloatArray();
-
-        // The vector name defaults to ""
-        return vectorsBuilder
-                .mergeFrom(namedVectors(Collections.singletonMap(options.vectorName, vector(embeddings)))).build();
-
+    if (options.embeddingField.isEmpty()) {
+      return vectorsBuilder.build();
     }
 
-    private static Vectors prepareSparseVectors(InternalRow record, StructType schema, QdrantOptions options) {
-        Map<String, Vector> sparseVectors = new HashMap<>();
+    int vectorFieldIndex = schema.fieldIndex(options.embeddingField);
+    float[] embeddings = record.getArray(vectorFieldIndex).toFloatArray();
 
-        for (int i = 0; i < options.sparseVectorNames.length; i++) {
-            String sparseVectorName = options.sparseVectorNames[i];
-            String sparseVectorValueField = options.sparseVectorValueFields[i];
-            String sparseVectorIndexField = options.sparseVectorIndexFields[i];
-            int sparseVectorValueFieldIndex = schema.fieldIndex(sparseVectorValueField.trim());
-            int sparseVectorIndexFieldIndex = schema.fieldIndex(sparseVectorIndexField.trim());
-            List<Float> sparseVectorValues = Floats.asList(record.getArray(sparseVectorValueFieldIndex).toFloatArray());
-            List<Integer> sparseVectorIndices = Ints.asList(record.getArray(sparseVectorIndexFieldIndex).toIntArray());
+    // The vector name defaults to ""
+    return vectorsBuilder
+        .mergeFrom(namedVectors(Collections.singletonMap(options.vectorName, vector(embeddings))))
+        .build();
+  }
 
-            sparseVectors.put(sparseVectorName, vector(sparseVectorValues, sparseVectorIndices));
-        }
+  private static Vectors prepareSparseVectors(
+      InternalRow record, StructType schema, QdrantOptions options) {
+    Map<String, Vector> sparseVectors = new HashMap<>();
 
-        return namedVectors(sparseVectors);
+    for (int i = 0; i < options.sparseVectorNames.length; i++) {
+      String sparseVectorName = options.sparseVectorNames[i];
+      String sparseVectorValueField = options.sparseVectorValueFields[i];
+      String sparseVectorIndexField = options.sparseVectorIndexFields[i];
+      int sparseVectorValueFieldIndex = schema.fieldIndex(sparseVectorValueField);
+      int sparseVectorIndexFieldIndex = schema.fieldIndex(sparseVectorIndexField);
+      List<Float> sparseVectorValues =
+          Floats.asList(record.getArray(sparseVectorValueFieldIndex).toFloatArray());
+      List<Integer> sparseVectorIndices =
+          Ints.asList(record.getArray(sparseVectorIndexFieldIndex).toIntArray());
+
+      sparseVectors.put(sparseVectorName, vector(sparseVectorValues, sparseVectorIndices));
     }
 
-    private static Vectors prepareDenseVectors(InternalRow record, StructType schema, QdrantOptions options) {
-        Map<String, Vector> denseVectors = new HashMap<>();
+    return namedVectors(sparseVectors);
+  }
 
-        for (int i = 0; i < options.vectorNames.length; i++) {
-            String vectorName = options.vectorNames[i];
-            String vectorField = options.vectorFields[i];
-            int vectorFieldIndex = schema.fieldIndex(vectorField.trim());
-            float[] vectorValues = record.getArray(vectorFieldIndex).toFloatArray();
+  private static Vectors prepareDenseVectors(
+      InternalRow record, StructType schema, QdrantOptions options) {
+    Map<String, Vector> denseVectors = new HashMap<>();
 
-            denseVectors.put(vectorName, vector(vectorValues));
-        }
+    for (int i = 0; i < options.vectorNames.length; i++) {
+      String vectorName = options.vectorNames[i];
+      String vectorField = options.vectorFields[i];
+      int vectorFieldIndex = schema.fieldIndex(vectorField);
+      float[] vectorValues = record.getArray(vectorFieldIndex).toFloatArray();
 
-        return namedVectors(denseVectors);
+      denseVectors.put(vectorName, vector(vectorValues));
     }
+
+    return namedVectors(denseVectors);
+  }
 }
