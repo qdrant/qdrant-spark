b5b198212de26ece4d2915f7e8a735e6ffd49c49
Anush008 <anushshetty90@gmail.com>
2024-03-23 00:17:56 +0530

    test: Java JSON source

--- src/test/java/io/qdrant/spark/TestIntegration.java
+++ src/test/java/io/qdrant/spark/TestIntegration.java
@@ -1,120 +1,65 @@
 package io.qdrant.spark;
 
-import io.qdrant.client.QdrantClient;
-import io.qdrant.client.QdrantGrpcClient;
-import io.qdrant.client.grpc.Collections.Distance;
-import io.qdrant.client.grpc.Collections.VectorParams;
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
+
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.Metadata;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.qdrant.QdrantContainer;
 
+import io.qdrant.client.QdrantClient;
+import io.qdrant.client.QdrantGrpcClient;
+import io.qdrant.client.grpc.Collections.Distance;
+import io.qdrant.client.grpc.Collections.VectorParams;
+
 @Testcontainers
 public class TestIntegration {
 
-  private static String collectionName = "qdrant-spark-".concat(UUID.randomUUID().toString());
-  private static int dimension = 4;
-  private static int grpcPort = 6334;
-  private static Distance distance = Distance.Cosine;
+    private static String collectionName = "qdrant-spark-".concat(UUID.randomUUID().toString());
+    private static int dimension = 4;
+    private static int grpcPort = 6334;
+    private static Distance distance = Distance.Cosine;
 
-  @Rule public final QdrantContainer qdrant = new QdrantContainer("qdrant/qdrant");
+    @Rule
+    public final QdrantContainer qdrant = new QdrantContainer("qdrant/qdrant");
 
-  @Before
-  public void setup() throws InterruptedException, ExecutionException {
+    @Before
+    public void setup() throws InterruptedException, ExecutionException {
 
-    QdrantClient client =
-        new QdrantClient(
+        QdrantClient client = new QdrantClient(
             QdrantGrpcClient.newBuilder(qdrant.getHost(), qdrant.getMappedPort(grpcPort), false)
-                .build());
+            .build());
 
-    client
-        .createCollectionAsync(
-            collectionName,
-            VectorParams.newBuilder().setDistance(distance).setSize(dimension).build())
-        .get();
+        client
+            .createCollectionAsync(
+                collectionName,
+                VectorParams.newBuilder().setDistance(distance).setSize(dimension).build())
+            .get();
 
-    client.close();
-  }
+        client.close();
+    }
 
-  @Test
-  public void testSparkSession() {
-    SparkSession spark =
-        SparkSession.builder().master("local[1]").appName("qdrant-spark").getOrCreate();
+    @Test
+    public void testSparkSession() {
+        SparkSession spark = SparkSession.builder().master("local[1]").appName("qdrant-spark").getOrCreate();
 
-    List<Row> data =
-        Arrays.asList(
-            RowFactory.create(
-                "3803333d-981d-4e76-8f08-f3ba72d006af",
-                1,
-                new float[] {1.0f, 2.0f, 3.0f, 0.3f},
-                "John Doe",
-                new String[] {"Hello", "Hi"},
-                RowFactory.create(99f, "AnotherNestedStruct"),
-                new int[] {4, 32, 323, 788}),
-            RowFactory.create(
-                "0ca61aca-c770-4f4e-9de6-41a58a98463b",
-                2,
-                new float[] {4.0f, 5.0f, 6.0f, 0.3f},
-                "Jane Doe",
-                new String[] {"Bonjour", "Salut"},
-                RowFactory.create(99f, "NestedStruct"),
-                new int[] {1, 2, 3, 4, 5}));
+        Dataset < Row > df = spark.read().schema(TestSchema.schema()).option("multiline", "true")
+            .json(this.getClass().getResource("/users.json").toString());
 
-    StructType structType =
-        new StructType(
-            new StructField[] {
-              new StructField("nested_id", DataTypes.FloatType, false, Metadata.empty()),
-              new StructField("nested_value", DataTypes.StringType, false, Metadata.empty())
-            });
-
-    StructType schema =
-        new StructType(
-            new StructField[] {
-              new StructField("id", DataTypes.StringType, false, Metadata.empty()),
-              new StructField("score", DataTypes.IntegerType, true, Metadata.empty()),
-              new StructField(
-                  "embedding",
-                  DataTypes.createArrayType(DataTypes.FloatType),
-                  true,
-                  Metadata.empty()),
-              new StructField("name", DataTypes.StringType, true, Metadata.empty()),
-              new StructField(
-                  "greetings",
-                  DataTypes.createArrayType(DataTypes.StringType),
-                  true,
-                  Metadata.empty()),
-              new StructField("struct_data", structType, true, Metadata.empty()),
-              new StructField(
-                  "numbers",
-                  DataTypes.createArrayType(DataTypes.IntegerType),
-                  true,
-                  Metadata.empty()),
-            });
-    Dataset<Row> df = spark.createDataFrame(data, schema);
-
-    String qdrantUrl = String.join("", "http://", qdrant.getGrpcHostAddress());
-    df.write()
-        .format("io.qdrant.spark.Qdrant")
-        .option("id_field", "id")
-        .option("schema", df.schema().json())
-        .option("collection_name", collectionName)
-        .option("embedding_field", "embedding")
-        .option("qdrant_url", qdrantUrl)
-        .mode("append")
-        .save();
-    ;
-  }
+        String qdrantUrl = String.join("", "http://", qdrant.getGrpcHostAddress());
+        df.write()
+            .format("io.qdrant.spark.Qdrant")
+            .option("id_field", "id")
+            .option("schema", df.schema().json())
+            .option("collection_name", collectionName)
+            .option("embedding_field", "dense_vector")
+            .option("qdrant_url", qdrantUrl)
+            .mode("append")
+            .save();
+    }
 }
---
+++ src/test/java/io/qdrant/spark/TestSchema.java
@@ -0,0 +1,90 @@
+package io.qdrant.spark;
+
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+public class TestSchema {
+
+  public static StructType schema() {
+    StructType hairSchema = new StructType()
+        .add(new StructField("color", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("type", DataTypes.StringType, true, Metadata.empty()));
+
+    StructType coordinatesSchema = new StructType()
+        .add(new StructField("lat", DataTypes.DoubleType, true, Metadata.empty()))
+        .add(new StructField("lng", DataTypes.DoubleType, true, Metadata.empty()));
+
+    StructType addressSchema = new StructType()
+        .add(new StructField("address", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("city", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("coordinates", coordinatesSchema, true, Metadata.empty()))
+        .add(new StructField("postalCode", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("state", DataTypes.StringType, true, Metadata.empty()));
+
+    StructType bankSchema = new StructType()
+        .add(new StructField("cardExpire", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("cardNumber", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("cardType", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("currency", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("iban", DataTypes.StringType, true, Metadata.empty()));
+
+    StructType companyAddressSchema = new StructType()
+        .add(new StructField("address", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("city", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("coordinates", coordinatesSchema, true, Metadata.empty()))
+        .add(new StructField("postalCode", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("state", DataTypes.StringType, true, Metadata.empty()));
+
+    StructType companySchema = new StructType()
+        .add(new StructField("address", companyAddressSchema, true, Metadata.empty()))
+        .add(new StructField("department", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("name", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("title", DataTypes.StringType, true, Metadata.empty()));
+
+    StructType cryptoSchema = new StructType()
+        .add(new StructField("coin", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("wallet", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("network", DataTypes.StringType, true, Metadata.empty()));
+
+    DataType denseVectorType = DataTypes.createArrayType(DataTypes.FloatType);
+    DataType sparseIndicesType = DataTypes.createArrayType(DataTypes.IntegerType);
+    DataType sparseValuesType = denseVectorType;
+
+    return new StructType()
+        .add(new StructField("id", DataTypes.IntegerType, true, Metadata.empty()))
+        .add(new StructField("firstName", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("lastName", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("maidenName", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("age", DataTypes.IntegerType, true, Metadata.empty()))
+        .add(new StructField("gender", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("email", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("phone", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("username", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("password", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("birthDate", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("image", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("bloodGroup", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("height", DataTypes.DoubleType, true, Metadata.empty()))
+        .add(new StructField("weight", DataTypes.DoubleType, true, Metadata.empty()))
+        .add(new StructField("eyeColor", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("hair", hairSchema, true, Metadata.empty()))
+        .add(new StructField("domain", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("ip", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("address", addressSchema, true, Metadata.empty()))
+        .add(new StructField("macAddress", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("university", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("bank", bankSchema, true, Metadata.empty()))
+        .add(new StructField("company", companySchema, true, Metadata.empty()))
+        .add(new StructField("ein", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("ssn", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("userAgent", DataTypes.StringType, true, Metadata.empty()))
+        .add(new StructField("crypto", cryptoSchema, true, Metadata.empty()))
+        .add(new StructField("dense_vector", denseVectorType, false, Metadata.empty()))
+        .add(new StructField("sparse_indices", sparseIndicesType, false, Metadata.empty()))
+        .add(new StructField("sparse_values", sparseValuesType, false, Metadata.empty()));
+
+  }
+}
