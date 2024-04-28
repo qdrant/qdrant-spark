e7f3bfd2c688f3e9a89f25e9658e84c087026d6a
Anush <anushshetty90@gmail.com>
2024-04-28 11:35:27 +0530

    chore: bump deps, refactor Py tests to use dict options (#25)
    * chore: bump deps
    
    * ci: bump pip
    
    * ci: remove 3.8 Python
    
    * Update README.md

--- src/test/python/conftest.py
+++ src/test/python/conftest.py
@@ -1,24 +1,26 @@
 import pytest
-from testcontainers.core.container import DockerContainer  # type: ignore
-from testcontainers.core.waiting_utils import wait_for_logs  # type: ignore
+from testcontainers.qdrant import QdrantContainer
 from qdrant_client import QdrantClient, models
 import uuid
 from pyspark.sql import SparkSession
 from typing import NamedTuple
+from uuid import uuid4
 
 
 QDRANT_GRPC_PORT = 6334
 QDRANT_EMBEDDING_DIM = 6
 QDRANT_DISTANCE = models.Distance.COSINE
+QDRANT_API_KEY = uuid4().hex
 
 
 class Qdrant(NamedTuple):
     url: str
+    api_key: str
     collection_name: str
     client: QdrantClient
 
 
-qdrant_container = DockerContainer("qdrant/qdrant").with_exposed_ports(QDRANT_GRPC_PORT)
+qdrant_container = QdrantContainer(image="qdrant/qdrant:latest", api_key=QDRANT_API_KEY)
 
 
 # Reference: https://gist.github.com/dizzythinks/f3bb37fd8ab1484bfec79d39ad8a92d3
@@ -36,9 +38,6 @@ def get_pom_version():
 @pytest.fixture(scope="module", autouse=True)
 def setup_container(request):
     qdrant_container.start()
-    wait_for_logs(
-        qdrant_container, ".*Actix runtime found; starting in Actix runtime.*", 60
-    )
 
     def remove_container():
         qdrant_container.stop()
@@ -70,6 +69,8 @@ def qdrant():
         host=host,
         grpc_port=grpc_port,
         prefer_grpc=True,
+        api_key=QDRANT_API_KEY,
+        https=False,
     )
 
     collection_name = str(uuid.uuid4())
@@ -99,6 +100,7 @@ def qdrant():
         url=f"http://{host}:{grpc_port}",
         client=client,
         collection_name=collection_name,
+        api_key=QDRANT_API_KEY,
     )
 
     return client.close()
